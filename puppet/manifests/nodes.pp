09877e05f1b35adb88529ca22d3ef8978ba8d492
vndr <jv.vndr@gmail.com>
2013-11-20 00:12:15 +0000

    ruby version manager module(blt04/puppet-rvm) added

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,19 +1,34 @@
 node 'default' {
- class { 'vagrant': }
   
-  class { 'virtualbox': }
-    
-  class { 'curl': }
+  stage { 'pre': before    => Stage['main'] }
 
+  class { 'user': stage => 'pre' }
+  
+  class { 'vagrant': }
+  class { 'virtualbox': }
+  class { 'curl': }
   class { 'googlechromebrowser': }
   class { 'skype': }
-  
   class { 'mysqlworkbench': }
-
   class { 'java': }
-
   class { 'tasksel': }
-  
   class { 'subversion': }
-  class { 'user': }
+
+  class { 'rvm': version => '1.24.1' }
+
+    rvm_system_ruby {
+      'ruby-1.9.3-p392':
+       ensure => 'present',
+       default_use => true;
+    }
+    
+    rvm_gem {
+        'bundler':
+        name => 'bundler',
+        ruby_version => 'ruby-1.9.3-p392',
+        ensure => latest,
+        require => Rvm_system_ruby['ruby-1.9.3-p392'];
+    }
+    
+    rvm::system_user { $::username:  }
 }
